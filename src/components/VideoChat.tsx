139f2cc053d99ae6b5909f4d48597fd2ae7598cd
bada308 <203190@jnu.ac.kr>
2024-06-26 23:25:14 +0900

    feat: Connect to the signaling server using a specific IP address and port

--- src/components/VideoChat.tsx
+++ src/components/VideoChat.tsx
@@ -13,7 +13,7 @@ const VideoChat = () => {
 
   useEffect(() => {
     // 시그널링 서버에 연결
-    const nextSocket = io(import.meta.env.VITE_SIGNAL_URL as string);
+    const nextSocket = io('http://43.200.1.142:8080/' as string);
     setSocket(nextSocket);
 
     // Google의 공개 STUN 서버를 사용하여 PeerConnection을 생성
